47dfeecb4ed2e34ead503abb6b07aba3d505c63a
shok1122 <shok1122@zohomail.com>
2020-12-09 10:02:29 +0000

    Added options for starting network and loading env.yaml

--- network.py
+++ network.py
@@ -3,10 +3,19 @@ import subprocess
 import shutil
 import sys
 import tarfile
+import yaml
 
 ENV_PATH = os.getenv('PATH')
 os.environ['PATH'] = os.getcwd() + '/bin:' + ENV_PATH
 os.environ['FABRIC_CFG_PATH'] = './conf'
+
+global_conf = None
+with open('env.yaml') as f:
+    global_conf = yaml.safe_load(f)
+
+g_domain = global_conf['domain']
+g_org = global_conf['org']
+g_peer = global_conf['peer']
 
 mode = sys.argv[1]
 
@@ -20,7 +29,9 @@ def install():
     # install docker images
     subprocess.call('script/install-fabric.sh docker', shell=True)
 
-def deploy(arcfile, docker_compose_file):
+def deploy():
+    arcfile = f'/tmp/{g_peer}-{g_org}.{g_domain}.tar.gz'
+    docker_compose_file = f'/tmp/docker-compose-{g_peer}-{g_org}.{g_domain}.yaml'
     # extract the fabric configuration files
     if not os.path.exists(arcfile):
         print(f'{arcfile} is not found...')
@@ -32,10 +43,18 @@ def deploy(arcfile, docker_compose_file):
     if not os.path.exists(docker_compose_file):
         print(f'{docker_compose_file} is not found...')
         return
-    shutil.copyfile(docker_compose_file, f'docker/{os.path.basename(docker_compose_file)}')
+    shutil.copyfile(docker_compose_file, f'docker/docker-compose.yaml')
+
+def network_up():
+    subprocess.call('docker-compose -f docker/docker-compose.yaml up -d', shell=True)
 
 if mode == 'install':
     install()
 elif mode == 'deploy':
-    deploy(sys.argv[2], sys.argv[3])
+    deploy()
+elif mode == 'up':
+    network_up()
+elif mode == 'all':
+    deploy()
+    network_up()
 

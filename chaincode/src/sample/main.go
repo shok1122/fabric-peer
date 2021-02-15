2b2445fdcb2aafaa53be0effb4d82605101de5de
shok1122 <shok1122@zohomail.com>
2021-02-15 07:56:04 +0000

    Added Printf for debug

--- chaincode/src/sample/main.go
+++ chaincode/src/sample/main.go
@@ -11,11 +11,15 @@ func main() {
 
     cc, err := contractapi.NewChaincode(ccCounter)
 
+    fmt.Printf("Start")
+
     if err != nil {
+        fmt.Printf("Error1")
         panic(err.Error())
     }
 
     if err := cc.Start(); err != nil {
+        fmt.Printf("Error2")
         panic(err.Error())
     }
 }

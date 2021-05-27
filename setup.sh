72267a676536adaac4455ae919b63ee006afda74
shok1122 <shok1122@zohomail.com>
2021-05-27 11:24:15 +0000

    Updated

--- setup.sh
+++ setup.sh
@@ -8,7 +8,9 @@ apt install -y \
     curl \
     software-properties-common
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+if [ ! -f /usr/share/keyrings/docker-archive-keyring.gpg ]; then
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+fi
 
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" > /etc/apt/sources.list.d/docker.list
 
@@ -18,4 +20,9 @@ apt install -y \
     docker-ce \
     docker-ce-cli \
     containerd.io
+
+if [ ! -f /usr/local/bin/docker-compose ]; then
+    sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    chmod +x /usr/local/bin/docker-compose
+fi
 

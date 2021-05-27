dd42da1328459a242a298ba615d4c9a70293ec32
shok1122 <shok1122@zohomail.com>
2021-05-27 11:13:44 +0000

    Added setup script for docker

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+apt update
+
+apt install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" > /etc/apt/sources.list.d/docker.list
+
+apt udpate
+
+apt install -y \
+    docker-ce \
+    docker-ce-cli \
+    containerd.io
+
